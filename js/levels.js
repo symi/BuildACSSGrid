ff02e289d9fc311263b31165f2b40be189b958af
Simon Levy <Simonjlevy@gmail.com>
2016-10-30 06:51:09 +0000

    sort of an example level...

--- js/levels.js
+++ js/levels.js
@@ -1,8 +1,18 @@
 (function () {
     window.levels = [
         {
-            blurb: 'some text',
-            winBlurbs: ['some text'],
+            blurb: `Hi there. Welcome to Build A CSS Grid, here you can learn new the CSS4 <pre>display: grid</pre> module!
+And have some target practice on some presidential candidates.
+
+<pre>display: grid</pre> - Defines the element as a grid container.
+<pre>grid-area</pre> - Names a set of elements in the grid.
+<pre>grid-template-areas</pre> - Defines an ASCII art style grid. Rows are defined in <pre>" "</pre>, with multiple rows separated by spaces defining columns. Possible values <pre>.</pre> for empty cell and <pre>{grid-area name}</pre> to position a named set.</pre>
+e.g. <pre>grid-template-areas: ". . ." ". item ."</pre>.`,
+            winBlurbs: [
+                '<pre>display: grid</pre>',
+                '<pre>grid-area</pre>',
+                '<pre>grid-template-areas</pre>'
+            ],
             code: `.container {
     display:grid;
 {{container|3}}
